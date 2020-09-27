bcbab828c9a44b59c5b231c45f543820631a4fa2
NguyenQuang99 <quanghero7@gmail.com>
2020-09-27 11:03:25 +0700

    BT_Luyentap/model/

---
+++ BT_Luyentap/service/Iservice.java
@@ -0,0 +1,11 @@
+package BT_Luyentap;
+
+public interface Iservice<P> {
+    void showMenu();
+    void addProduct();
+    void editProduce();
+    void delete();
+    void showProduct();
+    void searchProduct();
+    void sortProduct();
+}
